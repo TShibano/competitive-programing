f566ee6c03b9769a05aee3efe08ab66c27e50045
TShibano <46261399+TShibano@users.noreply.github.com>
2023-02-20 22:49:18 +0900

    クラスタリングの考察の途中.2023/02/20

--- atcoder/AHC/AHC018/src/main.jl
+++ atcoder/AHC/AHC018/src/main.jl
@@ -20,19 +20,23 @@ parse_int(str=readline()) = parse(Int64, str)
 
 
 # body ====
+#
+struct Coord
+    x::Int64
+    y::Int64
+end
 
 struct Input
     N::Int64
     W::Int64
     K::Int64
     C::Int64
-    Water::Matrix{Int64}
-    House::Matrix{Int64}
+    Water::Vector{Coord}
+    House::Vector{Coord}
 end
 
 struct Output
-    y::Int64
-    x::Int64
+    coord::Coord
     power::Int64
 end
 
@@ -47,38 +51,54 @@ end
 
 
 
+
 function parser_input()::Input
     N, W, K, C = parse_list_int()
-    water = Matrix{Int64}(undef, W, 2)
-    house = Matrix{Int64}(undef, K, 2)
+    water = Vector{Coord}(undef, W)
+    house = Vector{Coord}(undef, K)
     for i in 1:W
-        a, b = parse_list_int()
-        a += 1
-        b += 1
-        water[i, :] = [a, b]
+        y, x = parse_list_int()
+        y += 1
+        x += 1
+        water[i] = Coord(x, y)
     end
     for i in 1:K
-        c, d = parse_list_int()
-        c += 1
-        d += 1
-        house[i, :] = [c, d]
+        y, x = parse_list_int()
+        y += 1
+        x += 1
+        house[i, :] = Coord(x, y)
     end
     return Input(N, W, K, C, water, house)
 end
 
 
 function parser_output(output::Output)::Nothing
-    println(output.y, " ", output.x, " ", output.power)
+    println(output.coord.y, " ", output.coord.x, " ", output.power)
 end
 
+"""TODO"""
 function check_result(res::ExeResult)
     if res.r == -1
         error("Output Error: r is strange.")
     elseif r == 0
+        pass
     elseif r == 1
+        pass
     elseif r ==2
+        pass
     else
         error("Output Error: r is out of range.")
+    end
+end
+
+"""2点のL1距離(マンハッタン距離)を求める"""
+function calc_l1(coord1::Coord, coord2::Coord)::Int64
+    return abs(coord1.x - coord2.x) + abs(coord1.y - coord2.y)
+end
+
+"""水源にL1距離が最も近い水源を探す"""
+function greed_clustering(input::Input)
+    
 end
 
 
