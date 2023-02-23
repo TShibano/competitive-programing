e84c1c0b2af5c4958ee9b8ee3733f423b0cd1c05
TShibano <46261399+TShibano@users.noreply.github.com>
2023-02-23 18:47:09 +0900

    version 1.1.1

--- atcoder/AHC/AHC018/src/solve10.jl
+++ atcoder/AHC/AHC018/src/solve10.jl
@@ -1,31 +1,16 @@
 # AHC018 - main.jl
 # Date: 2023/02/20
-# Version: 1.1.1
+# Version: 1.0.0
 #== Solution
-#diff
-v1.0
-- 最初の解法
-v1.1
-- 頑丈さを見ながら掘るパワーを決める
-- 頑丈さは，周りの値のパワーから推定する
-v1.1.1
-- 推定する範囲を1マスから2マスに広げた
-
-結局まっすぐ進むので方向見て頑丈さを推定するべきか
 ==#
 
 # library ====
-using Random
 # using Primes
 # using DataStructures
 
 # const ====
 const MOD = 10^9 + 7
 const INF = 10^14
-const DX = [1, 1, 1, -1, -1, -1, 0, 0]
-const DY = [1, 0, -1, 1, 0, -1, 1, -1]
-const DX2 = [2, 2, 2, 2, 2, 1, 1, 1, 1, 1, 0, 0, 0, 0, -1, -1, -1, -1, -1, -2, -2, -2, -2, -2]
-const DY2 = [2, 1, 0, -1, -2, 2, 1, 0, -1, -2, 2, 1, -1, -2, 2, 1, 0, -1, -2, 2, 1, 0, -1, -2]
 
 # templete function ====
 parse_list_int(str=readline()) = parse.(Int64, split(str))
@@ -72,12 +57,10 @@ end
 mutable struct Field
     is_crushed::Matrix{Bool}
     sturdiness::Matrix{Int64}
-    now_power::Matrix{Int64}
     function Field(input::Input)
         is_crushed = falses(input.N, input.N)
-        sturdiness = fill(100, input.N, input.N)
-        now_power = zeros(Int64, input.N, input.N)
-        new(is_crushed, sturdiness, now_power)
+        sturdiness = zeros(Int64, input.N, input.N)
+        new(is_crushed, sturdiness)
     end
 end
 
@@ -86,37 +69,16 @@ function check_field(field::Field, coord::Coord)::Bool
     return field.is_crushed[coord.y, coord.x]
 end
 
-function get_strudiness(field::Field, coord::Coord)::Int64
-    return field.sturdiness[coord.y, coord.x]
-end
-
-function get_now_power(field::Field, coord::Coord)::Int64
-    return field.now_power[coord.y, coord.x]
-end
-
-function record_crush!(field::Field, coord::Coord)::Nothing
+function crushed_at_coord!(field::Field, coord::Coord)::Nothing
     field.is_crushed[coord.y, coord.x] = true
     return nothing
 end
 
-"""TODO"""
-function update_field!(field::Field, coord::Coord, power::Int64)::Nothing
-    # 周囲のaroundマスの分の頑丈さを更新する
-    y = coord.y
-    x = coord.x
-    for i in 1:24       # 周囲2マスを探索(5^2 -1)
-        ny = y + DY2[i]
-        nx = x + DX2[i]
-        if 1 <= ny <= 200 && 1 <= nx <= 200    # 200は固定なので手打ちする
-            update_val = 0.1*(power - field.sturdiness[ny, nx])
-            update_val = trunc(Int64, update_val)
-            field.sturdiness[ny, nx] += update_val
-        else
-            continue
-        end
-    end
+function update!(field::Field, coord::Coord, power::Int64)::Nothing
+    field.sturdiness[coord.y, coord.x] += power
     return nothing
 end
+
 
 function parser_input()::Input
     N, W, K, C = parse_list_int()
@@ -155,6 +117,20 @@ function interactive(output::Output)::Result
     return parser_input_interactive()
 end
 
+"""TODO"""
+function check_result(res::Result)
+    if res.r == -1
+        error("Output Error: r is strange.")
+    elseif r == 0
+        pass
+    elseif r == 1
+        pass
+    elseif r ==2
+        pass
+    else
+        error("Output Error: r is out of range.")
+    end
+end
 
 """2点のL1距離(マンハッタン距離)を求める"""
 function calc_l1(coord1::Coord, coord2::Coord)::Int64
@@ -204,40 +180,19 @@ function calc_center(coord_list::Vector{Coord})::Coord
 end
 
 
-"""ある座標の岩盤を壊す
-壊すまで掘り続ける．
-使ったパワーを返す
-1回で掘れたら力を入れすぎていたということで，返すパワーを小さくする
-"""
-function crush!(field::Field, coord::Coord)::Int64
-    total_power = 0
-    total_cnt = 0
+"""壊す座標"""
+function crush!(field::Field, coord::Coord, power::Int64)::Bool
     if check_field(field, coord)    # すでに壊されている場合
-        return total_power
-    end
-    while true
-        total_cnt += 1
-        # 掘る力は推論した頑丈さを使って掘る
-        power = get_strudiness(field, coord) - get_now_power(field, coord)
-        if power <= 0   # パワーが負になった場合適当にパワーを入れる
-            power = 100
-        elseif power < 10    # 頑丈さは10以上
-            power = 10
-        elseif power > 5000   # 頑丈さは5000以下
-            power = 5000
-        end
-        total_power += power
-        # 出力し，結果を受け取る
-        res::Result = interactive(Output(coord, power))
+        return true
+    else
+        output = Output(coord, power)
+        update!(field, coord, power)    # 頑丈さの更新
+        res::Result = interactive(output)
         if res.r == 0       # 指定した座標の岩盤はまだ壊れていない
-            continue
+            return false
         elseif res.r == 1   # 指定した座標の岩盤が壊れた
-            record_crush!(field, coord)      # 岩盤が壊れたあことを記録する
-            #update_crush!(field, coord)
-            if total_cnt == 1
-                total_power = trunc(Int64, total_power*0.8)
-            end
-            return total_power
+            crushed_at_coord!(field, coord)      # 
+            return true
         elseif res.r == 2   # すぐに終了する
             exit()
         end
@@ -245,10 +200,10 @@ function crush!(field::Field, coord::Coord)::Int64
 end
 
 """パスを見つける
-(old)ver. 1.0: 単純にスタート地点とゴール地点を結ぶ
-(now)ver. 1.1: ゴール地点もしくは近くのすでに掘られている地点を探しに行く
+(now)ver. 1.0: 単純にスタート地点とゴール地点を結ぶ
+ver. 1.1: ゴール地点もしくは近くのすでに掘られている地点を探しに行く
 """
-function find_path10(s_coord, g_coord, field)::Vector{Coord}
+function find_path(s_coord, g_coord, field)::Vector{Coord}
     path = Coord[]
     # y軸方向をまず決める
     if s_coord.y <= g_coord.y
@@ -273,26 +228,27 @@ function find_path10(s_coord, g_coord, field)::Vector{Coord}
     return path
 end
 
-
-
 """スタート地点とゴール地点を与えて掘り進める"""
-function crush_goal!(s_coord::Coord, g_coord::Coord, field::Field)
+function crush_goal!(s_coord::Coord, g_coord::Coord, power::Int64, field::Field)
     #println("Now crush_goal!")
-    # ルートを見つける
-    path = find_path10(s_coord, g_coord, field)
+    path = find_path(s_coord, g_coord, field)
     #println("#", path)
     for i in 1:length(path)
         now_coord = path[i]
-        # すでに壊れている場合は壊さない
         if check_field(field, now_coord)
             continue
         end
-        total_power = crush!(field, now_coord)
-        update_field!(field, now_coord, total_power)
+        while true
+            response = crush!(field, now_coord, power)
+            if response     # 指定した座標が壊れている場合次に行く
+                break
+            end
+        end
     end
 end
 
 function solve1_0(input::Input, field::Field)
+    power = 100
     # クラスタリングする
     clusters = clustering_nearing_water(input)
     # クラスタリングしたグループ内での重心を求める
@@ -320,11 +276,11 @@ function solve1_0(input::Input, field::Field)
         g_coord = target_coord_list[w]    # このグループの目標となる座標(重心)
         s_coord = input.Water[w]
         # 水源から目標地点に掘り進める
-        crush_goal!(s_coord, g_coord, field)
+        crush_goal!(s_coord, g_coord, power, field)
         # 家から目標地点に掘り進める
         for i in 1:n
             s_coord = input.House[clusters[w][i]]
-            crush_goal!(s_coord, g_coord, field)
+            crush_goal!(s_coord, g_coord, power, field)
         end
     end
     println("# 最後まできた")
